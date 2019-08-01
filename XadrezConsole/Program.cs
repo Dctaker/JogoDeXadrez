19b87c136893b4837ac3e3f3efc5c4e5bf4b3b8d
Bruno Carvalho <brunocarvalhoviana@outlook.com>
2019-08-01 11:26:20 -0300

    Criado Classe Posicao

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
-
+using Tabuleiro;
 namespace XadrezConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-           
+           Posicao p = new Posicao(3, 4);
+
+            Console.WriteLine("Posição " + p);
+            Console.ReadKey();
         }
     }
 }
---
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Posicao
+    {
+
+        //2.1 - Propiedades da Classe
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        //2.2 - Construtor da Classe
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+
+        //2.3 - Metodo Para Imprimir 
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+
+    }
+}
